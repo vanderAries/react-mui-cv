e41520099f9c5dc1f9f2ccc765207d8c4d7a7ad6
vanderAries <gp.bak96@gmail.com>
2024-02-20 21:35:17 +0100

    Skills component

---
+++ src/components/atoms/EducationItem/index.tsx
@@ -0,0 +1,31 @@
+import { Typography, Box } from "@mui/material";
+
+interface EducationItemProps {
+  years: string;
+  university: string;
+  degree: string;
+  field: string;
+  detail: string;
+}
+
+export default function EducationItem({
+  years,
+  university,
+  degree,
+  field,
+  detail,
+}: EducationItemProps) {
+  return (
+    <Box display="flex" flexDirection="column" marginY={1}>
+      <Box>
+        <Typography variant="subtitle1">{years}</Typography>
+        <Typography variant="h4">{university}</Typography>
+      </Box>
+      <Box marginY={1}>
+        <Typography variant="body1">{degree}</Typography>
+        <Typography variant="body1">{field}</Typography>
+      </Box>
+      <Typography variant="body1">{detail}</Typography>
+    </Box>
+  );
+}
